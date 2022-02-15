7db2c835f6bb2bd4eb6b54bdc74b61cd5122f6f6
wramow <wil11251991>
2022-02-14 22:32:36 -0700

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const [generatedMbiId, setGeneratedMbi] = useState(null);
+  const [verificationStatus, setVerificationStatus] = useState(null);
+  const [verifyMbiInput, setVerifyMbiInput] = useState("")
+
+  function generateMBI()
+  {
+    fetch('http://localhost:5000/generate', {
+      mode: 'cors',
+      method: 'GET'
+    })
+    .then( results => results.json() )
+    .then( data => setGeneratedMbi(data))
+    .catch( function(error) {console.log(error)} )
+  }
+
+  function verifyMBI(event)
+  {
+    fetch('http://localhost:5000/verify', {
+      mode: 'cors',
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        'mbi' : verifyMbiInput
+      })
+    })
+    .then((response) => {
+      if(response.ok) {
+        return response.json();
+      }
+      else {
+        throw new Error("Something went wrong with verification")
+      }
+      })
+    .then( data => setVerificationStatus(data))
+    .catch( function(error) {console.log(error)} )  }
+
+
   return (
-    <div className="App">
+    <div className="App"> Generate/Verify MBI
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={generateMBI} type='submit'>Generate MBI</button>
+          <input readOnly={true} value={generatedMbiId} ></input>
+          <br/>
+          <button onClick={verifyMBI}>Verify MBI</button>
+          <input onChange={e => setVerifyMbiInput(e.target.value)}></input>
+          <div>{verificationStatus}</div>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
